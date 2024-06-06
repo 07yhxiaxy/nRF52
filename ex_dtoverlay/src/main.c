86a603b935c59f79466042b112d61343df24e542
Mark <07yhxixay@gmail.com>
2024-06-06 17:22:10 -0400

    updated exercise

--- ex_dtoverlay/src/main.c
+++ ex_dtoverlay/src/main.c
@@ -1,10 +1,15 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
+#ifdef CONFIG_MYFUNC
+#include "myfunc.h"
+#endif
 
 int main(void)
 {
+	int a = 3, b = 4;
 	while(1){
 		printk("Hello World!\n\r");
+		printk("a is %d, b is %d, the sum is %d\n\r", a, b, sum(a,b));
 		k_msleep(1000);
 	}
 }
